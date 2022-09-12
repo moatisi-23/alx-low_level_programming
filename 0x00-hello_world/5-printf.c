57c508baafffc02753c425339d0456b8724fc31c
moatisi-23 <moatisi.rampomane@gmail.com>
2022-09-12 14:19:54 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,12 @@
 /**
 * main - print the string in the put function
 *
-* Destination: using the main function
-* this program prints "Programming is like building a multilingual puzzle"
+* Description: using the main function
+* this program prints "Programming is like building a mulilingual puzzle"
 * Return: 0
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
